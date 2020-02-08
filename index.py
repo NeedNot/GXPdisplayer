51ce72dafc43e59f698e663b9cb1bf096f9ad8e5
Need_Not <47156725+NeedNot@users.noreply.github.com>
2020-02-07 17:14:24 -0800

    Update index.py

--- index.py
+++ index.py
@@ -1,10 +1,13 @@
 import requests
 
-r = requests.get('PRIVET URL')
+r = requests.get('https://api.hypixel.net/guild?key=PRIVET&name=GUILDNAME')
 x = r.json()
 
 member0 = x['guild']['members'][0]
 
+
+uuidURL = "https://sessionserver.mojang.com/session/minecraft/profile/" + member0['uuid']
+uuidURL = requests.get(uuidURL).json()
 
 total0 = 0
 for exp_earned in member0['expHistory'].values():
@@ -12,4 +15,16 @@ for exp_earned in member0['expHistory'].values():
 total0 = '{:,}'.format(total0)
 
 
-print("%s past 7 days GXP: %s." % (member0['uuid'], total0))
+print("%s past 7 days GXP: %s." % (uuidURL['name'], total0))
+
+member1 = x['guild']['members'][1]
+uuidURL = "https://sessionserver.mojang.com/session/minecraft/profile/" + member1['uuid']
+uuidURL = requests.get(uuidURL).json()
+
+total1 = 0
+for exp_earned in member1['expHistory'].values():
+    total1 += exp_earned
+total1 = '{:,}'.format(total1)
+
+
+print("%s past 7 days GXP: %s." % (uuidURL['name'], total1))
