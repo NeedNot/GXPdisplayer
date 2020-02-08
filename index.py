b40bed7e5178157b5248ad24c6c8d0ff2062532c
Need_Not <47156725+NeedNot@users.noreply.github.com>
2020-02-08 07:20:33 -0800

    Update index.py

--- index.py
+++ index.py
@@ -1,39 +1,14 @@
 import requests
 
-r = requests.get('https://api.hypixel.net/guild?keyKEY5&name=GUILD')
-x = r.json()
-
-if len(x['guild']['members']) == 0:
-  sys.exit()
-
-member0 = x['guild']['members'][0]
-print(len(x['guild']['members']))
-
-uuidURL = "https://sessionserver.mojang.com/session/minecraft/profile/" + member0['uuid']
-uuidURL = requests.get(uuidURL).json()
-
-total0 = 0
-for exp_earned in member0['expHistory'].values():
-    total0 += exp_earned
-total0 = '{:,}'.format(total0)
+g = requests.get('https://api.hypixel.net/guild?key=APIKEY&name=GUILD NAME').json()
 
 
-print("%s past 7 days GXP: %s." % (uuidURL['name'], total0))
 
+for i in range(len(g['guild']['members'])):
+  uuid = g['guild']['members'][i]['uuid']
 
-total1 = 0
+  x = requests.get("https://sessionserver.mojang.com/session/minecraft/profile/" + uuid)
+  x = x.json()
 
-if len(x['guild']['members']) == 0:
-  sys.exit()
-
-member1 = x['guild']['members'][1]
-uuidURL = "https://sessionserver.mojang.com/session/minecraft/profile/" + member1['uuid']
-uuidURL = requests.get(uuidURL).json()
-
-
-for exp_earned in member1['expHistory'].values():
-    total1 += exp_earned
-total1 = '{:,}'.format(total1)
-
-
-print("%s past 7 days GXP: %s." % (uuidURL['name'], total1))
+  expHistory = g['guild']['members'][i]['expHistory']
+  print(f"{x['name']} Last 7 Days Guild XP {sum(expHistory.values())}\n")
